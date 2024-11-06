7234b2d713f987d48730d688e3b870c6db8b8743
oshivimarsha <oshadivimarsha@gmail.com>
2024-11-06 12:12:47 +0530

    change Signup page

--- src/main/java/lk/ijse/controller/SignUpFormController.java
+++ src/main/java/lk/ijse/controller/SignUpFormController.java
@@ -3,6 +3,8 @@ package lk.ijse.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -10,6 +12,10 @@ import java.io.IOException;
 
 public class SignUpFormController {
     public AnchorPane rootNode;
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public PasswordField txtEmail;
+    public PasswordField txtPosition;
 
     public void btnBackToPreview(ActionEvent actionEvent) throws IOException {
         AnchorPane rootNode = FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"));
